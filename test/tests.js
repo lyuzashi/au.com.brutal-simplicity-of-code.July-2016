ac916c71c8a8d0bd5cebdaaaae2f6d1b458b2990
Kevin Brown <Kevin.Brown@make.com.au>
2016-07-18 12:18:33 +1000

    Style and arrow function clean up.

--- test/tests.js
+++ test/tests.js
@@ -9,12 +9,11 @@ var expect = chai.expect;
 
 describe('Oppish Generator', function() {
 
-    it('should generate Hello World correctly', function() {
+    it('should generate Hello World correctly', () => {
         expect(Oppish.generate('Hello World')).to.equal('H-opp-ell-opp-o W-opp-or-opp-l-opp-d-opp');
     });
 
     it('should handle small words correctly', () => {
-      expect(Oppish.generate('This is small')).to.equal('T-opp-h-opp-is-opp opp-is s-opp-m-opp-all-opp');
-    })
-
+        expect(Oppish.generate('This is small')).to.equal('T-opp-h-opp-is-opp opp-is s-opp-m-opp-all-opp');
+    });
 });
