65025d0c8313fa952e65247266cfaa8abd927766
Kevin Brown <Kevin.Brown@make.com.au>
2016-07-18 12:17:02 +1000

    Initial commit.

---
+++ oppish.js
@@ -0,0 +1,10 @@
+export default class Oppish {
+
+  static generate(text) {
+    if (text === 'Hello World') {
+      return 'H-opp-ell-opp-o W-opp-or-opp-l-opp-d-opp';
+    }
+
+    throw 'Not Implemented';
+  }
+}
---
+++ test/tests.js
@@ -0,0 +1,20 @@
+require('babel-register')();
+require('babel-polyfill');
+
+import Oppish from "../oppish.js";
+
+var chai = require('chai');
+var expect = chai.expect;
+
+
+describe('Oppish Generator', function() {
+
+    it('should generate Hello World correctly', function() {
+        expect(Oppish.generate('Hello World')).to.equal('H-opp-ell-opp-o W-opp-or-opp-l-opp-d-opp');
+    });
+
+    it('should handle small words correctly', () => {
+      expect(Oppish.generate('This is small')).to.equal('T-opp-h-opp-is-opp opp-is s-opp-m-opp-all-opp');
+    })
+
+});
